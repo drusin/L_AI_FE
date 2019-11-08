2af78a4ea4810dc0e5f4c196fd9abf2f0680378a
Simon Lenz <dev@simon-lenz.de>
2019-11-08 23:41:41 +0100

    load spider sprite

--- src/scenes/main_menu.ts
+++ src/scenes/main_menu.ts
@@ -2,6 +2,8 @@
 import { Scene } from 'phaser';
 import Box2d from './box2d';
 
+import spiderBotSpritesheet from '../assets/sprites/Spider.png';
+
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
     active: true,
     visible: true,
@@ -15,10 +17,20 @@ export class MainMenuScene extends Scene {
         super(sceneConfig);
     }
 
+    preload() {
+        this.load.spritesheet(
+            'spider-bot', 
+            spiderBotSpritesheet, 
+            { frameWidth: 32, frameHeight: 32 }
+        );
+    }
+
     create() {
         this.add.text(100, 50, 'Welcome!', { fill: '#FFFFFF' }).setFontSize(24);
         this.box2d = new Box2d();
-        this.box2d.createStaticBox(10, 10, 100, 5);
+        this.box2d.createStaticBox(10, 30, 100, 25);
+        this.box2d.createStaticBox(100, 300, 17, 76);
+        this.add.image(400, 300, 'spider-bot');
     }
 
     update(time: number, delta: number): void {
